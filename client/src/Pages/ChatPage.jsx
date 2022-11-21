7a11569177a859b0b102ff49caae5aeb3a6477b6
Victor Machado <48452755+victorsmachado@users.noreply.github.com>
2022-11-21 08:23:38 -0300

    Add files via upload

--- client/src/Pages/ChatPage.jsx
+++ client/src/Pages/ChatPage.jsx
@@ -1,12 +1,31 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+
 import { ChatState } from "../Context/ChatProvider";
+import { Box } from "@chakra-ui/react";
+import SideDrawer from "../components/Miscellaneous/SideDrawer";
+import MyChats from "../components/Chat/MyChats";
+import ChatBox from "../components/Chat/ChatBox";
 
 const ChatPage = () => {
-  const {user} = ChatState()
-  return <div style={{width: '100%'}}>
-    
-  </div>;
+  const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
+  return (
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box
+        display="flex"
+        justifyContent={"space-between"}
+        w="100%"
+        h={"91.5vh"}
+        p="10px"
+      >
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
+      </Box>
+    </div>
+  );
 };
 
 export default ChatPage;
